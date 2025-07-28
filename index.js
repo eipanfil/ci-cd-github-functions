c0f8a3d025fdf206efbe40a4f73f40d09ab64c9e
Evgenij Panfilov <kinloguert@yandex-team.ru>
2025-07-29 02:19:17 +0700

    test 2 index js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ exports.handler = async function (event, context) {
 
   // Проверяем наличие "hello" без учета регистра
   if (inputStr.toLowerCase().includes('hello')) {
-      result = "Hello, world!1";
+      result = "Hello, world!ct";
   } else {
       result = "Nothing to say!";
   }
